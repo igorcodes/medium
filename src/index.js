0a0be3f211d22aea349cca489a839833e3733a6a
Igor88git <zakutajloigor@gmail.com>
2022-02-08 12:24:19 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+import TopBar from 'components/topBar'
+//у нас абсолютные пути, все пути начинаются от нод модулес
+import {CurrentUserProvider} from 'context/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {BrowserRouter as Router} from 'react-router-dom';
+import Routes from 'routes';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// обычная функция с разметкой внутри
+const App = () => {
+	return (
+		<CurrentUserProvider>
+			<CurrentUserChecker>
+			{/* <h3>welcome to hooks</h3> */}
+			<Router>
+				<TopBar />
+				<Routes/>
+			</Router>
+			</CurrentUserChecker>
+		</CurrentUserProvider>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
